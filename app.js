c25ec8fc4dc0b0b5ba81f257b75ec941fb081c56
kassotakis <kassotakisnikolas@gmail.com>
2025-07-08 01:25:54 +0300

    added health path

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 /* eslint-env node */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.listen(PORT)
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT);
