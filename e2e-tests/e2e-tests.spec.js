0ac4e44a568511b5adb1182c11ac7f97fad57f9d
kassotakis <kassotakisnikolas@gmail.com>
2025-07-07 08:58:55 +0300

    added test

--- e2e-tests/e2e-tests.spec.js
+++ e2e-tests/e2e-tests.spec.js
@@ -11,3 +11,13 @@ describe("Pokedex", () => {
     ).toBeVisible();
   });
 });
+
+test("one can navigate from the main page to the page of a particular Pokemon", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:8080");
+
+  await page.getByText("ivysaur").click();
+
+  await expect(page.getByText("chlorophyll")).toBeVisible();
+});
