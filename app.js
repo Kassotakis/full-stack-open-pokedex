8a7d008c02f4a6c36e027536e557d8900515c176
kassotakis <kassotakisnikolas@gmail.com>
2025-07-07 05:33:26 +0300

    11.6 fixes eslint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 const express = require("express");
 const app = express();
 
@@ -6,6 +7,4 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+app.listen(PORT);
